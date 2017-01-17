fe150447f6d797bf1180ec6bb56b4666b9464800
Vasu-p <vasudevp_24@yahoo.in>
2017-01-17 16:57:09 +0530

    Made changes to the path from middleware_mvc to -middleware_SMC

--- views/header.php
+++ views/header.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/middleware_mvc">SMC Data Middleware</a>
+            <a class="navbar-brand" href="/-middleware_SMC">SMC Data Middleware</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
